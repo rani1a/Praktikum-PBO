4f0fb75df8d337d9b67d14aaaf463fc8415b35b4
Rania <160609219+rani1a@users.noreply.github.com>
2024-02-28 10:05:40 +0700

    Update MGaris.java

--- Praktikum 2/src/MGaris.java
+++ Praktikum 2/src/MGaris.java
@@ -12,6 +12,5 @@ public class MGaris {
 Kesimpulan : Jika kita merubah acces modifier pada atribut menjadi public maka nilai atribut bisa menjadi tidak konsisten,
 maka dari itu sebaiknya atribut diset private supaya hanya diakses oleh class itu sendiri. Untuk konstruktor harus
 diset public supaya bisa untuk membuat objek di luar class. Sedangkan, untuk method sebaiknya di set public supaya bisa
-dipanggil. Misalnya, method refleksiX dan refleksiY harus di set public supaya bisa dipanggil di class lain yaitu di class
-main.
+dipanggil. Garis mempunyai hubungan agregasi dengan Titik, karena Garis bergantung pada Titik. Garis sebagai Client dan Titik sebagai Supplier
  */
