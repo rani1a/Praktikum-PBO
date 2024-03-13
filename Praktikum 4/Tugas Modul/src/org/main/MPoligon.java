6f188bdd540cfe771f773871538757764dfd6d3c
Rania <160609219+rani1a@users.noreply.github.com>
2024-03-13 16:48:11 +0700

    Add files via upload
    Rania - 24060122120013

---
+++ Praktikum 4/Tugas Modul/src/org/main/MPoligon.java
@@ -0,0 +1,19 @@
+/**
+ * File : MPoligon.java  13/03/2024
+ * Penulis : Rania
+ * NIM : 24060122120013
+ * Deskripsi : Driver class untuk poligon dan persegi panjang
+ */
+
+package org.main;
+import  org.bangundatar.*;
+public class MPoligon {
+    public static void main(String[] args){
+        PersegiPanjang persegi = new PersegiPanjang(10,10,4);
+        persegi.printInfo();
+        System.out.println("Luas Persegi Panjang : " + persegi.hitungLuas());
+        Segitiga segitiga = new Segitiga(10,10,3);
+        segitiga.printInfo();
+        System.out.println("Luas Segitiga : " + segitiga.hitungLuas());
+    }
+}
---
+++ Praktikum 4/Tugas Modul/src/org/poligon/Poligon.java
@@ -0,0 +1,15 @@
+/**
+ * File : Poligon.java  13/03/2024
+ * Penulis : Rania
+ * NIM : 24060122120013
+ * Deskripsi : Representasi dasar dari objek poligon (segi banyak)
+ */
+package org.poligon;
+
+public class Poligon {
+    protected int jumlahSisi;
+
+    public int getJumlahSisi() {
+        return this.jumlahSisi;
+    }
+}
