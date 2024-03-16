3d2dd4857e935a00783a86f074baacb2c2c1c532
Rania <160609219+rani1a@users.noreply.github.com>
2024-03-16 21:58:49 +0700

    Add files via upload
    Rania - 24060122120013

---
+++ Praktikum 4/Tugas Tambahan/src/org/mobil/Mobil.java
@@ -0,0 +1,37 @@
+package org.mobil;
+import org.kendaraan.Kendaraan;
+
+public class Mobil extends Kendaraan {
+    private int jumlahPintu;
+
+    public Mobil(String nama, int jumlahPintu) {
+        super(nama);
+        this.jumlahPintu = jumlahPintu;
+    }
+
+    public int getJumlahPintu() {
+        return jumlahPintu;
+    }
+
+    public void setJumlahPintu(int jumlahPintu) {
+        this.jumlahPintu = jumlahPintu;
+    }
+    @Override
+    public void cetakInfo() {
+        super.cetakInfo();
+        System.out.println("Jenis kendaraan: Mobil");
+        System.out.println("Jumlah pintu: " + jumlahPintu);
+    }
+
+    public void bukaPintu(int nomorPintu) {
+        if (nomorPintu <= getJumlahPintu()) {
+            System.out.println("Pintu nomor " + nomorPintu + " di mobil telah terbuka.");
+        } else {
+            System.out.println("Tidak ada pintu ke" + nomorPintu + "pada mobil.");
+        }
+    }
+
+    public void klakson() {
+        System.out.println(super.getNama() + " berbunyi, Vroom Vroom!");
+    }
+}
